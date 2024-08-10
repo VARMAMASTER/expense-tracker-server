fc10f9bd65604b5ab3ea96d8d28cc0506886c036
sai kiran <saikiran@janaspandana.in>
2024-08-10 21:17:16 +0530

    updated verce-json

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,7 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PassportModule } from '@nestjs/passport';
-import { User, UserSchema } from 'src/schemas/user';
+import { User, UserSchema } from 'src/schemas/user-schema';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtStrategy } from './jwt/jwt.strategy';
