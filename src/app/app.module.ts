f634e1dc8c93f436aab8db50c287b957a86db3dc
sai kiran <saikiran@janaspandana.in>
2024-08-10 21:35:00 +0530

    updated verce-json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from '../auth/auth.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+//
 @Module({
   imports: [
     ConfigModule.forRoot({
