91f5b0f9edca7c6f6d98eccb7e4e4824681be6ac
sai kiran <saikiran@janaspandana.in>
2024-08-10 21:04:13 +0530

    updated verce-json

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
-import { MongooseModule } from '@nestjs/mongoose';
 import { PassportModule } from '@nestjs/passport';
-import { User, UserSchema } from 'src/schemas/user';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtStrategy } from './jwt/jwt.strategy';
@@ -22,7 +20,7 @@ import { JwtStrategy } from './jwt/jwt.strategy';
       secret: process.env.JWT_SECRET,
       signOptions: { expiresIn: process.env.JWT_EXPIRES_IN },
     }),
-    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    // MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
   ],
   exports: [AuthService],
   controllers: [AuthController],
