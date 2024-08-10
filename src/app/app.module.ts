db0c9eb33a2d95149bc63ce484089a3a7faf3079
sai kiran <saikiran@janaspandana.in>
2024-08-10 20:59:44 +0530

    updated verce-json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-import { User, UserSchema } from 'src/schemas/user';
 import { AuthModule } from '../auth/auth.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -14,7 +13,7 @@ import { AppService } from './app.service';
     }),
     MongooseModule.forRoot(process.env.MONGO_URI),
     AuthModule,
-    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    // MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,96 +1,86 @@
-import {
-  BadRequestException,
-  HttpException,
-  Injectable,
-  UnauthorizedException,
-} from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-import { InjectModel } from '@nestjs/mongoose';
-import * as bcrypt from 'bcrypt';
-import { Model } from 'mongoose';
-import { User, UserDocument } from 'src/schemas/user';
-import { createUserDto, loginUserDto } from './dto/auth.dto';
 
 @Injectable()
 export class AuthService {
   constructor(
     private readonly jwtService: JwtService,
-    @InjectModel(User.name) private readonly userModel: Model<UserDocument>,
+    // @InjectModel(User.name) private readonly userModel: Model<UserDocument>,
   ) {}
 
-  async signUp(body: createUserDto): Promise<UserDocument | any> {
-    try {
-      const { email, phoneNumber } = body;
+  // async signUp(body: createUserDto): Promise<UserDocument | any> {
+  //   try {
+  //     const { email, phoneNumber } = body;
 
-      // Check if a user with the same email or phone number already exists
-      const existingUser = await this.findExistingUser(email, phoneNumber);
-      if (existingUser) {
-        throw new BadRequestException(
-          'A user with this email or mobile number already exists',
-        );
-      }
+  //     // Check if a user with the same email or phone number already exists
+  //     const existingUser = await this.findExistingUser(email, phoneNumber);
+  //     if (existingUser) {
+  //       throw new BadRequestException(
+  //         'A user with this email or mobile number already exists',
+  //       );
+  //     }
 
-      // Create and save the new user
-      const createUser = new this.userModel(body);
-      const userData = await createUser.save();
-      return {
-        success: true,
-        message: 'User created successfully',
-        access_token: this.jwtService.sign({
-          id: userData.id,
-          userId: userData.id,
-        }),
-        data: userData,
-      };
-    } catch (error) {
-      console.log(error);
-      throw new HttpException(error.message, error.status);
-    }
-  }
+  //     // Create and save the new user
+  //     const createUser = new this.userModel(body);
+  //     const userData = await createUser.save();
+  //     return {
+  //       success: true,
+  //       message: 'User created successfully',
+  //       access_token: this.jwtService.sign({
+  //         id: userData.id,
+  //         userId: userData.id,
+  //       }),
+  //       data: userData,
+  //     };
+  //   } catch (error) {
+  //     console.log(error);
+  //     throw new HttpException(error.message, error.status);
+  //   }
+  // }
 
-  private async findExistingUser(
-    email: string,
-    phoneNumber: string,
-  ): Promise<UserDocument | null> {
-    return await this.userModel.findOne({
-      $or: [{ email }, { phoneNumber }],
-    });
-  }
+  // private async findExistingUser(
+  //   email: string,
+  //   phoneNumber: string,
+  // ): Promise<UserDocument | null> {
+  //   return await this.userModel.findOne({
+  //     $or: [{ email }, { phoneNumber }],
+  //   });
+  // }
 
-  async login(body: loginUserDto) {
-    try {
-      const { email, password } = body;
+  // async login(body: loginUserDto) {
+  //   try {
+  //     const { email, password } = body;
 
-      // Find the user by email
-      const existingUser = await this.userModel.findOne({ email });
-      if (!existingUser) {
-        throw new BadRequestException('Invalid login credentials');
-      }
+  //     // Find the user by email
+  //     const existingUser = await this.userModel.findOne({ email });
+  //     if (!existingUser) {
+  //       throw new BadRequestException('Invalid login credentials');
+  //     }
 
-      // Compare the provided password with the hashed password
-      const isPasswordValid = await bcrypt.compare(
-        password,
-        existingUser.password,
-      );
-      if (!isPasswordValid) {
-        throw new UnauthorizedException('Invalid login password');
-      }
+  //     // Compare the provided password with the hashed password
+  //     const isPasswordValid = await bcrypt.compare(
+  //       password,
+  //       existingUser.password,
+  //     );
+  //     if (!isPasswordValid) {
+  //       throw new UnauthorizedException('Invalid login password');
+  //     }
 
-      // Generate a JWT token if the login is successful
-      const token = this.jwtService.sign({
-        id: existingUser.id,
-        userId: existingUser.id,
-      });
+  //     // Generate a JWT token if the login is successful
+  //     const token = this.jwtService.sign({
+  //       id: existingUser.id,
+  //       userId: existingUser.id,
+  //     });
 
-      return {
-        success: true,
-        message: 'Login successful',
-        access_token: token,
-        data: existingUser,
-      };
-    } catch (error) {
-      console.log(error);
-      throw new HttpException(error.message, error.status || 500);
-    }
-  }
+  //     return {
+  //       success: true,
+  //       message: 'Login successful',
+  //       access_token: token,
+  //       data: existingUser,
+  //     };
+  //   } catch (error) {
+  //     console.log(error);
+  //     throw new HttpException(error.message, error.status || 500);
+  //   }
+  // }
 }
