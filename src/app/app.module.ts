f20164b725dc679a85872ba440b5b19c26f4dc4c
sai kiran <saikiran@janaspandana.in>
2024-08-10 18:08:37 +0530

    updated structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from 'src/schemas/user';
 import { AuthModule } from '../auth/auth.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -13,6 +14,7 @@ import { AppService } from './app.service';
     }),
     MongooseModule.forRoot(process.env.MONGO_URI),
     AuthModule,
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
   ],
   controllers: [AppController],
   providers: [AppService],
