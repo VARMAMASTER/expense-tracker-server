b2d78f7feb5b5670c75561a35f5270f20f0e2f20
sai kiran <saikiran@janaspandana.in>
2024-08-17 13:50:19 +0530

    minor text changes

--- multi-app/src/main.ts
+++ multi-app/src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Enable CORS with a single line configuration
+  // Enable CORS
   app.enableCors({ origin: '*' });
 
   // Apply global validation pipe
